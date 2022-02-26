5cdcebdad47d92b11068d3a34eef83a9dfc5e615
Shambhavi Jha <89500052+shambhaviijhaa@users.noreply.github.com>
2022-02-26 21:53:23 +0530

    Add files via upload

---
+++ Task2/Q4.dart
@@ -0,0 +1,32 @@
+void main()
+{
+ 
+  var answer= new Map();
+  answer[1]='a';
+  answer[2]='b';
+  answer[3]='c';
+  answer[4]='d';
+  answer[5]='e';
+  answer[6]='f';
+  answer[7]='g';
+  answer[8]='h';
+  answer[9]='i';
+  answer[10]='j';
+  answer[11]='k';
+  answer[12]='l';
+  answer[13]='m';
+  answer[14]='n';
+  answer[15]='o';
+  answer[16]='p';
+  answer[17]='q';
+  answer[18]='r';
+  answer[19]='s';
+  answer[20]='t';
+  answer[21]='u';
+  answer[22]='v';
+  answer[23]='w';
+  answer[24]='x';
+  answer[25]='y';
+  answer[26]='z';
+  print(answer);
+}
